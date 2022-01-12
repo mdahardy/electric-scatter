4ac0be815b5802abcc3a9fba2952423cd956d887
Mathew Hardy <matt.hardy@mail.utoronto.ca>
2022-01-12 17:04:21 -0500

    Updating density video and fixing root js and css

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,12 @@
-document.querySelectorAll(".video-div").forEach(function(div_wrapper){
-    const curr_video_div = document.getElementById(`video-${div_wrapper.id.slice(-1)}`);
-    div_wrapper.addEventListener('mouseover',function(){
-        curr_video_div.play();
+if (!(window.matchMedia("(any-pointer:coarse)").matches)){
+    document.querySelectorAll('.project-video source').forEach(source_i => {source_i.setAttribute('src', source_i.getAttribute('data-src'))});
+    document.querySelectorAll('.project-video').forEach(vid => {vid.load()});
+    document.querySelectorAll(".video-div").forEach(function(div_wrapper){
+        const curr_video_div = document.getElementById(`video-${div_wrapper.id.slice(-1)}`);
+        div_wrapper.addEventListener('mouseover',()=>{curr_video_div.play()});
+        div_wrapper.addEventListener('mouseleave',()=>{
+            curr_video_div.currentTime = 0;
+            curr_video_div.pause();
+        });
     });
-    div_wrapper.addEventListener('mouseleave',function(){
-        curr_video_div.currentTime = 0;
-        curr_video_div.pause();
-    });
-})
+};
